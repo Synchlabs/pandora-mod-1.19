790b64b2757d90233a2e7a43426631d251a64937
Synchlabs <v0pt2.0@gmail.com>
2022-09-26 21:52:18 -0400

    Initial commit

---
+++ src/main/java/net/synchlabs/pandora/PandoraClient.java
@@ -0,0 +1,10 @@
+package net.synchlabs.pandora;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PandoraClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/synchlabs/pandora/PandoraMod.java
@@ -0,0 +1,14 @@
+package net.synchlabs.pandora;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PandoraMod implements ModInitializer {
+	public static final String MOD_ID = "pandora";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize(){
+	}
+}
