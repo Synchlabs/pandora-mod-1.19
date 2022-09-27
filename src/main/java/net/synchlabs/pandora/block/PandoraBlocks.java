b66582d0b86ac02dc2bc5775c0c10313aca54416
Synchlabs <v0pt2.0@gmail.com>
2022-09-27 03:56:09 -0400

    Added Unobtanium Ore, which does not yet generate naturally. Can only be mined with netherite, and will be an extremely rare Ender ore.

--- src/main/java/net/synchlabs/pandora/block/PandoraBlocks.java
+++ src/main/java/net/synchlabs/pandora/block/PandoraBlocks.java
@@ -1,8 +1,31 @@
 package net.synchlabs.pandora.block;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.block.OreBlock;
+import net.minecraft.item.*;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
+import net.minecraft.util.registry.Registry;
 import net.synchlabs.pandora.PandoraMod;
+import net.synchlabs.pandora.item.PandoraItemGroup;
 
 public class PandoraBlocks {
+    public static final Block UNOBTANIUM_ORE = registerBlock("unobtanium_ore",
+            new OreBlock(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool(), UniformIntProvider.create(9, 22)), PandoraItemGroup.PANDORA_BLOCKS);
+
+    private static Block registerBlock(String name, Block block, ItemGroup tab) {
+        registerBlockItem(name, block, tab);
+        return Registry.register(Registry.BLOCK, new Identifier(PandoraMod.MOD_ID, name), block);
+    }
+
+    private static Item registerBlockItem(String name, Block block, ItemGroup tab) {
+        return Registry.register(Registry.ITEM, new Identifier(PandoraMod.MOD_ID, name),
+                new BlockItem(block, new FabricItemSettings().group(tab)));
+    }
+
 
     public static void registerModBlocks() {
         PandoraMod.LOGGER.debug("Registering Mod Blocks for " + PandoraMod.MOD_ID);
--- src/main/java/net/synchlabs/pandora/item/PandoraItemGroup.java
+++ src/main/java/net/synchlabs/pandora/item/PandoraItemGroup.java
@@ -5,9 +5,12 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import net.synchlabs.pandora.PandoraMod;
+import net.synchlabs.pandora.block.PandoraBlocks;
 
 public class PandoraItemGroup {
     public static final ItemGroup PANDORA_ITEMS = FabricItemGroupBuilder.build(
             new Identifier(PandoraMod.MOD_ID, "pandora_items"), () -> new ItemStack(PandoraItems.UNOBTANIUM_FRAGMENT));
+    public static final ItemGroup PANDORA_BLOCKS = FabricItemGroupBuilder.build(
+            new Identifier(PandoraMod.MOD_ID, "pandora_blocks"), () -> new ItemStack(PandoraBlocks.UNOBTANIUM_ORE));
 
 }
